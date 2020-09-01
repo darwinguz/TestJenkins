aecb76c5b80ed46768c9df11e98b94efc14c3451
Darwin Guzmán <dga_guz@hotmail.es>
2020-08-31 21:27:53 -0500

    Punto y coma agregado

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ class Main{
 		System.out.println("Hola mundo!");
 		System.out.println("Prueba para GitHub y Jenkins");
 		System.out.println("Prueba desde la rama develop");
-		System.out.println("Prueba de error desde la rama master")
+		System.out.println("Prueba de error desde la rama master");
 	}
 }
